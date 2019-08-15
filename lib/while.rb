d6fdde62b232f02331f4bef20b49e44add69011d
Sara McCombs <thesaramccombs@gmail.com>
2019-08-15 19:19:14 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
 def using_while
   levitation_force = 6
 	
-  #your code here
+  #method will stop printing the phrase when the levitation_force == 10
+  
+  while levitation_force < 10 do
+    puts 
+  end
 end
 
 
